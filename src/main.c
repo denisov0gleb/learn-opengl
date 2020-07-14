0eebac08672695afa921c5f2e82745d34e976fae
DenisovGL <denisov0gleb@gmail.com>
2020-07-14 22:09:02 +0300

    Create simple shaders; add info from 'docs.gl'; now triangle is red [dgl]

--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <malloc.h>
+
 #include "GLEW/glew.h"
 #include "GLFW/glfw3.h"
 
@@ -13,6 +15,63 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 }
 
 
+static unsigned int CompileShader(unsigned int type, const char* source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = &source[0];
+
+/*
+ * OpenGL copies the shader source code strings when glShaderSource is called,
+ * so an application may free its copy of the source code strings immediately after the function returns.
+ */
+	glShaderSource(id, 1, &src, NULL);
+	glCompileShader(id);
+
+	/*
+	 * Simple error handling:
+	 */
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+
+	/* Or if (!result) */
+	if (result == GL_FALSE)
+	{
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*) malloc(sizeof(char) * length);
+
+		glGetShaderInfoLog(id, length, &length, message);
+
+		fprintf(stdout, ">>> ERROR >>> Failed to compile %s shader!\n", type == GL_VERTEX_SHADER ? "vertex" : "fragment");
+		fprintf(stdout, ">>> ERROR >>> Message:\n%s\n", message);
+		glDeleteShader(id);
+
+		return 0;
+	}
+
+	return id;
+}
+
+
+static unsigned int CreateShader(const char* vertexShader, const char* fragmentShader)
+{
+	/* Or use GLuint */
+	unsigned int program = glCreateProgram();
+	unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -20,7 +79,10 @@ int main(void)
 
 	/* Initialize the library */
 	if (!glfwInit())
+	{
+		fprintf(stdout, ">>> ERROR >>> Cannot initialize GLFW3!\n");
 		return -1;
+	}
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -41,7 +103,7 @@ int main(void)
 		return -1;
 	}
 
-	fprintf(stdout, "GL version:\n%s", glGetString(GL_VERSION));
+	fprintf(stdout, ">>> INFO >>> GL version: %s", glGetString(GL_VERSION));
 	
 
 /*
@@ -62,19 +124,96 @@ int main(void)
  */
 
 	unsigned int buffer;
+
+	/*
+	 * Triangle vertexes posotions:
+	 */
 	float positions[6] = {
 		-0.5f, -0.5f,
 		0.0f, 0.5f,
 		0.5f, -0.5f
 	};
 
+
+/*
+ * void glGenBuffers(GLsizei n, GLuint* buffers);
+ * returns n buffer object names in buffers
+ */
 	glGenBuffers(1, &buffer);
+
+/*
+ * void glBindBuffer(GLenum target, GLuint buffer);
+ * Buffer Binding Target        	 Purpose
+ * GL_ARRAY_BUFFER              	 Vertex attributes
+ * GL_ATOMIC_COUNTER_BUFFER     	 Atomic counter storage
+ * GL_COPY_READ_BUFFER          	 Buffer copy source
+ * GL_COPY_WRITE_BUFFER         	 Buffer copy destination
+ * GL_DISPATCH_INDIRECT_BUFFER  	 Indirect compute dispatch commands
+ * GL_DRAW_INDIRECT_BUFFER      	 Indirect command arguments
+ * GL_ELEMENT_ARRAY_BUFFER      	 Vertex array indices
+ * GL_PIXEL_PACK_BUFFER         	 Pixel read target
+ * GL_PIXEL_UNPACK_BUFFER       	 Texture data source
+ * GL_QUERY_BUFFER              	 Query result buffer
+ * GL_SHADER_STORAGE_BUFFER     	 Read-write storage for shaders
+ * GL_TEXTURE_BUFFER            	 Texture data buffer
+ * GL_TRANSFORM_FEEDBACK_BUFFER 	 Transform feedback buffer
+ * GL_UNIFORM_BUFFER            	 Uniform block storage
+ */
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+
+/*
+ * glBufferData - creates and initializes a buffer object's data store
+ * void glBufferData(GLenum target,
+ *                   GLsizeiptr size,
+ *                   const GLvoid* data,
+ *                   GLenum usage);
+ * Usage:
+ * GL_STREAM_DRAW, GL_STREAM_READ, GL_STREAM_COPY, GL_STATIC_DRAW, GL_STATIC_READ, GL_STATIC_COPY, GL_DYNAMIC_DRAW, GL_DYNAMIC_READ, or GL_DYNAMIC_COPY
+ */
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0);
+
+/*
+ * glVertexAttribPointer — define an array of generic vertex attribute data
+ * void glVertexAttribPointer(GLuint index,
+ *                           GLint size,
+ *                           GLenum type,
+ *                           GLboolean normalized,
+ *                           GLsizei stride,
+ *                           const GLvoid * pointer);
+ */
 	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+	/*
+	 * Vertex and fragment Shaders:
+	 */
+	const char* vertexShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) in vec4 position;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = position;\n"
+		"}\n";
+
+
+	const char* fragmentShader =
+		"#version 330 core\n"
+		"\n"
+		"layout(location = 0) out vec4 color;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"	color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"}\n";
+
+
+	unsigned int shader = CreateShader(vertexShader, fragmentShader);
+	glUseProgram(shader);
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -82,6 +221,16 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+/*
+ * void glDrawArrays(GLenum mode,
+ *                   GLint first, // Specifies the starting index in the enabled arrays
+ *                   GLsizei count); // Specifies the number of indices to be rendered
+ * 
+ * Modes:
+ * 	GL_POINTS, GL_LINE_STRIP, GL_LINE_LOOP, GL_LINES, GL_LINE_STRIP_ADJACENCY,
+ * 	GL_LINES_ADJACENCY, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN, GL_TRIANGLES,
+ * 	GL_TRIANGLE_STRIP_ADJACENCY, GL_TRIANGLES_ADJACENCY and GL_PATCHES
+ */
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
